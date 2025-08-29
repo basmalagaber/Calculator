948f7b3da958a12fff8ed6af56cb7c6444070cb3
basmala gaber <basmalagaber049@gmail.com>
2025-08-30 00:03:01 +0300

    new change

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'Calculator/calculator_screen.dart';
 import 'Magazin/magazin_page.dart';
+import 'XO/xo_game.dart';
+import 'XO/xo_main_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +16,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CalculatorScreen(),
+      initialRoute: XoMainScreen.routeName,
+      routes: {
+        XoMainScreen.routeName:(context) => XoMainScreen(),
+        XoGame.routeName:(context) => XoGame(),
+      },
     );
   }
 }
